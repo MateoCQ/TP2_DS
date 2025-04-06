dc8853878daa11b8cbfda0e29a0ae4f8e450db14
Mateo Cortez Quintino <mateocortezquintino@gmail.com>
2025-04-05 22:01:59 -0300

    Añadido componente PasswordInput

---
+++ src/components/PasswordInput.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+import "../App.css"
+export function PasswordInput(){
+        const [password, setPassword] = useState("")
+        const [showPassword, setShowPassword] = useState(false)
+
+return(
+    <div className="Input_Container">
+        <input type={showPassword ? "text": "password"} value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Ingrese una contraseña"/>
+        
+        <button onClick={() => setShowPassword(!showPassword)}>
+            {showPassword ? <EyeOff size={20}/> : <Eye size={20}/>} 
+        </button>
+    </div>
+    )
+}
